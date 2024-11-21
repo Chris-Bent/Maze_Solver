27eaf58e6348c679a8e8a3b81d38ae25770c45da
Chris-Bent <chriswjbent@gmail.com>
2024-11-21 14:20:54 +0000

    break

--- window.py
+++ window.py
@@ -2,5 +2,16 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.window = Tk()
-        window.title("Maze Solver")
+        super().__init__(self, width, height)
+        
+        # Create Tk window
+        self.__root = Tk()
+        __root.title("Maze Solver")
+        
+        # Creating a screen that can expand
+        # to the size of the window
+        self.screen = Canvas(self.__root)
+        __root.pack(fill = BOTH, expand = True)
+
+        # Running window. Set to false
+        self.running = False
